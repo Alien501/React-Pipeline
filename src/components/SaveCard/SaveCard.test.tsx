6508f93947bf774199a1602260d99a4b0c955361
Alien501 <cvignesh404@gmail.com>
2025-02-26 21:18:00 +0530

    Tests should work now

--- src/components/SaveCard/SaveCard.test.tsx
+++ src/components/SaveCard/SaveCard.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import SaveCard from './SaveCard';
 import { useLyricsStore } from '../../store/useLyricsStore';
 import { useMusicStore } from '../../store/useMusicStore';
